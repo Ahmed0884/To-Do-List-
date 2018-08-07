ae3c35dc04cdc445a700ea47196084beab263b06
Ahmed nabil <ahmednabil@Ahmeds-Mac.local>
2018-08-07 12:47:27 -0700

    first work todolist

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -8,8 +8,43 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+   var list = ["buy clothes","study english","go to gym","attend training"]
 
+
+class FirstViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+
+    
+    @IBOutlet weak var mytable: UITableView!
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (list.count)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default , reuseIdentifier: "cell")
+        cell.textLabel?.text = list [indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.delete
+        {
+        
+          list.remove(at: indexPath.row)
+            mytable.reloadData()
+    
+        }
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        mytable.reloadData()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    
+    @IBOutlet weak var outout: UITextField!
+    
+    
+    @IBAction func Additem(_ sender: Any) {
+        
+        
+        if (outout.text != "")
+        
+        {
+        
+            list.append(outout.text!)
+            outout.text = ""
+            
+        
+        }
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
